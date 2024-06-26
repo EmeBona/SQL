13fd7b3ba1745af80f5ee615a0bb55c912f29b76
María Bona <161622351+EmeBona@users.noreply.github.com>
2024-06-26 10:00:41 +0200

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,20 @@
+-- TABLE
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    author VARCHAR(200) NOT NULL,
+    genre VARCHAR(200),
+    published_year INT,
+    isbn VARCHAR(20) UNIQUE,
+    price DECIMAL(10, 2),
+    rating DECIMAL(3, 2),
+    stock_count INT
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
